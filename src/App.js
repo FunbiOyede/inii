508e08a026dfa886c84a765f18b6a2559556832e
FunbiOyede <oyedetobi49@gmail.com>
2019-08-05 07:19:10 +0100

    test ci and cd pipeline

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>hellow.js</code> and save to reload.
+          welcome to inii bookmarks
         </p>
         <a
           className="App-link"
