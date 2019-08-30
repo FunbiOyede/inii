4e977327ff83bcb1c40eceeead5392bb39935f57
FunbiOyede <oyedetobi49@gmail.com>
2019-08-30 18:14:38 +0100

    error 404 page

--- src/component/Error/Error.jsx
+++ src/component/Error/Error.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
-const Error = (props) => (
-    <div>
-        <h1>Error 404 </h1>
-        <h4>Page not found</h4>
-    </div>
-)
+const Error = props => (
+  <div>
+    <h1>Error 404 </h1>
+    <h4>Page not found</h4>
+  </div>
+);
 
 export default Error;
