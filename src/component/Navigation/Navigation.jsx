a58956768b3b95fbd04b11c13c0225a0939f4769
FunbiOyede <oyedetobi49@gmail.com>
2019-09-12 13:05:54 +0100

    side bar menu

--- src/component/Navigation/Navigation.jsx
+++ src/component/Navigation/Navigation.jsx
@@ -1,50 +1,79 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navigation.module.css";
 import SideDrawer from "../SideDrawer/SideDrawer";
+import { Icon } from "antd";
 
-const Navigation = props => (
-  <header className={styles.Header}>
-    <nav>
-      <div>
-        <div className={styles.Logo}>
-          {/* <Link to="/home" style={{ color: "white" }}>
+class Navigation extends Component {
+  state = {
+    Drawer: false
+  };
+  Drawer = () => {
+    this.setState({
+      Drawer: !this.state.Drawer
+    });
+  };
+
+  render() {
+    return (
+      <header className={styles.Header}>
+        <nav>
+          <div>
+            <div className={styles.Logo}>
+              {/* <Link to="/home" style={{ color: "white" }}>
             logo
           </Link> */}
-          <SideDrawer />
-        </div>
-        <ul className={styles.navItems}>
-          <li>
-            <Link
-              to="/AddBookmark"
-              style={{
-                color: "white",
-                padding: "7px 16px",
-                borderRadius: "4px",
-                background: "#00000080"
-              }}
-            >
-              Add Bookmark
-            </Link>
-          </li>
+              <SideDrawer SideBar={this.state.Drawer} />
+            </div>
+            <div>
+              <Icon
+                type="align-center"
+                style={{
+                  color: "white",
+                  position: "relative",
+                  fontSize: "33px",
+                  zIndex: "20px",
+                  top: "11px",
+                  outline: "none",
+                  left: "7px"
+                }}
+                onClick={this.Drawer}
+              />
+            </div>
+            <ul className={styles.navItems}>
+              <li>
+                <Link
+                  to="/AddBookmark"
+                  style={{
+                    color: "white",
+                    padding: "7px 16px",
+                    borderRadius: "4px",
+                    background: "#00000080"
+                  }}
+                >
+                  Add Bookmark
+                </Link>
+              </li>
 
-          <li>
-            <Link
-              to="/"
-              style={{
-                color: "white",
-                padding: "7px 16px",
-                borderRadius: "4px",
-                background: "#00000080"
-              }}
-            >
-              Signout
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-);
+              <li>
+                <Link
+                  to="/"
+                  style={{
+                    color: "white",
+                    padding: "7px 16px",
+                    borderRadius: "4px",
+                    background: "#00000080"
+                  }}
+                >
+                  Signout
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
 
 export default Navigation;
--- src/component/SideDrawer/SideDrawer.jsx
+++ src/component/SideDrawer/SideDrawer.jsx
@@ -2,16 +2,14 @@ import React, { Component } from "react";
 import styles from "./SideDrawer.module.css";
 
 class SideDrawer extends Component {
-  Drawer = () => {
-    console.log("hellow");
-  };
-
   render() {
     return (
-      <div onClick={this.Drawer} className={styles.SideDrawer}>
-        <div className={styles.FirstBar}></div>
-        <div className={styles.SecondBar}></div>
-        <div className={styles.ThirdBar}></div>
+      <div
+        className={this.props.SideBar ? styles.CloseDrawer : styles.SideDrawer}
+      >
+        <div>
+          <h3>Categories</h3>
+        </div>
       </div>
     );
   }
