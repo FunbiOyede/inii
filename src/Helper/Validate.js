992722212eb26572d75f34b9197cf2184dc1f91e
FunbiOyede <oyedetobi49@gmail.com>
2019-09-16 11:59:17 +0100

    remove escape characters from email regex

--- src/Helper/Validate.js
+++ src/Helper/Validate.js
@@ -13,7 +13,7 @@ const ValidateUrl = value => {
 };
 
 export const ValidateEmail = value => {
-  const expression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const expression = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const regex = new RegExp(expression);
   let isValid = true;
   if (value.match(regex)) {
