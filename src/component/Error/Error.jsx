0b385853e9bc3d4e9acb3504388d36353f2dd62d
FunbiOyede <oyedetobi49@gmail.com>
2019-09-22 21:10:39 +0100

    make Error 404 page responsive to mobile devices

--- src/component/Error/Error.jsx
+++ src/component/Error/Error.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import ErrorPage from "../../assets/56128677966243.5c978f7bcf385.png";
+import styles from "./Error.module.css";
 
 const Error = props => (
   <div>
-    <img
-      src={ErrorPage}
-      alt="page not found"
-      style={{
-        position: "relative",
-        left: "334px",
-        top: "30px"
-      }}
-    />
+    <img src={ErrorPage} alt="page not found" className={styles.Error} />
   </div>
 );
 
