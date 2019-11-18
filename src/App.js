856ec81a98a8032c9015bc32c173ad7bd0e78709
FunbiOyede <oyedetobi49@gmail.com>
2019-11-18 22:41:51 +0100

    fixed store bug

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-import Layout from "./component/Layout/Layout";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import AddBookmark from "./container/NewBookmark/AddBookmark";
+import Home from "././component/Home/Home";
+import Error from "././component/Error/Error";
+import LandingPage from "././component/LandingPage/LandingPage";
+import Login from "././component/Login/Login";
+import Register from "././component/Register/Register";
 
+import { connect } from "react-redux";
 class App extends Component {
   render() {
+    let routerGaurds = (
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <Redirect to="/" />
+      </Switch>
+    );
+    if (this.props.isAuthtenticated) {
+      routerGaurds = (
+        <Switch>
+          <Route path="/home" exact component={Home} />
+          <Route path="/AddBookmark" exact component={AddBookmark} />
+          <Route component={Error} />
+        </Switch>
+      );
+    }
+
     return (
       <div>
         <BrowserRouter>
-          <div>
-            <Layout />
-          </div>
+          <div>{routerGaurds}</div>
         </BrowserRouter>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthtenticated: state.token != null
+  };
+};
+
+export default connect(mapStateToProps)(App);
